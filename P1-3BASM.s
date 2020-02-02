025e7bf717a8c54488e198058575f722b3dbc306
Kaleb Byrum <kabyru@outlook.com>
2020-02-02 14:48:40 -0500

    Comment modifications

--- P1-3BASM.s
+++ P1-3BASM.s
@@ -1,9 +1,9 @@
-//Assembly Subroutine
-//Written by Kaleb Byrum, code by Eugene Rockey, 2018.
+/* Assembly Subroutine */
+/* Written by Kaleb Byrum, code by Eugene Rockey, 2018. */
 	.section ".text"
 	.global next_char
 next_char:
-	ADD r0,#1 //How is the ARM calling convention obeyed here?
+	ADD r0,#1 /* How is the ARM calling convention obeyed here? */
 	MOV pc,lr
 	.end
 
